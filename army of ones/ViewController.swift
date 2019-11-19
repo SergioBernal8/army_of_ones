1feb31ff070017177da97425646568efd98c88b5
Sergio Bernal Reyes <sbernal@belatrixsf.com>
2019-11-19 10:05:31 -0500

    first commit

---
+++ army of ones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  army of ones
+//
+//  Created by Sergio Bernal Reyes on 11/19/19.
+//  Copyright © 2019 Sergio Bernal Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
